7815ff8b9f21274ef95b3ca066fc71618dfa13a3
rizwan <mac@macs-MacBook-Pro.local>
2024-04-28 21:35:26 +0500

    add env configration in next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    BASE_URL_API: process.env.BASE_URL_API,
+    ENCRYPTION_SECRET: process.env.ENCRYPTION_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+};
 
 export default nextConfig;
